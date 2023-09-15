0c20cc26cd71efea55b05c774161569a329c7608
Idedav <xdavide.correa@gmail.com>
2023-09-15 16:07:44 +0200

    [add] input message on html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-const wordUser = prompt('inserire una parola')
+// WORD PALINDROME
+const wordUserInput = document.getElementById('word-user')
+const btnCheck = document.getElementById('btn-check')
 let message;
-if(palindrome(wordUser) === true){
-    message ='La parola inserita è palindroma'
-}else{
-    message ='La parola inserita NON è palindroma'
-}
+btnCheck.addEventListener('click', function(){
+    const wordUser = wordUserInput.value;
+    wordUserInput.value= ''
+    if(palindrome(wordUser) === true){
+        message ='La parola inserita è palindroma'
+    }else{
+        message ='La parola inserita NON è palindroma'
+    }
+    document.getElementById('palindrome-output').innerHTML= message
+})
 
 
 
-
+// FUNCTION PALINDROME
 function palindrome(word){
     const wordReverse = word.split('').reverse().join('');
     return word === wordReverse
